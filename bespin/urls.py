d255a22c8d18dd46b4591b16d3287dbfd8c96b91
Dan Leehr <dan.leehr@duke.edu>
2016-10-25 16:07:28 -0400

    Remove unused auth_views import

--- bespin/urls.py
+++ bespin/urls.py
@@ -1,8 +1,5 @@
-
 from django.conf.urls import url, include
 from django.contrib import admin
-from django.contrib.auth import views as auth_views
-
 
 urlpatterns = [
     url(r'^accounts/', include('django.contrib.auth.urls')),
