857e55e99da2bf196f9bbba5a243affb4aaa0ffd
Dan Leehr <dan.leehr@duke.edu>
2016-11-07 11:18:13 -0500

    Includes api-auth urls

--- data/urls.py
+++ data/urls.py
@@ -7,4 +7,6 @@ router.register(r'projects', api.ProjectsViewSet, 'project')
 
 urlpatterns = [
     url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls',
+                               namespace='rest_framework')),
 ]
