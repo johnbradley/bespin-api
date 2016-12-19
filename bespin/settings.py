a3fe6254d91dbef68b2e80b9e0c601d4b1965e57
Dan Leehr <dan.leehr@duke.edu>
2016-12-19 15:43:22 -0500

    Adds JSONRenderer to settings.py and raises default page size
    - JSONRenderer can be activated with a header of "Accept: application/json" and JSONAPI Renderer by default or with "Application/vnd.api+json"

--- bespin/settings.py
+++ bespin/settings.py
@@ -139,7 +139,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ),
-    'PAGE_SIZE': 10,
+    'PAGE_SIZE': 20,
     'EXCEPTION_HANDLER': 'rest_framework_json_api.exceptions.exception_handler',
     'DEFAULT_PAGINATION_CLASS':
         'rest_framework_json_api.pagination.PageNumberPagination',
@@ -150,6 +150,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework_json_api.renderers.JSONRenderer',
+        'rest_framework.renderers.JSONRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer',
     ),
     'DEFAULT_METADATA_CLASS': 'rest_framework_json_api.metadata.JSONAPIMetadata',
