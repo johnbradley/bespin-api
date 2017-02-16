93a20f26dbae0f6f95f68bba95be8edf9388484e
John Bradley <johnbradley2008@gmail.com>
2017-02-16 13:27:37 -0500

    makeusertoken field name change

--- data/management/commands/makeusertoken.py
+++ data/management/commands/makeusertoken.py
@@ -8,13 +8,13 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument('username')
         parser.add_argument('password')
-        parser.add_argument('tokenname')
+        parser.add_argument('tokenkey')
 
     def handle(self, **options):
         username = options['username']
         password = options['password']
-        tokenname = options['tokenname']
+        tokenkey = options['tokenkey']
         user = User.objects.create(username=username, is_staff=True, is_superuser=True)
         user.set_password(password)
         user.save()
-        t = Token.objects.create(key=tokenname, user=user)
+        t = Token.objects.create(key=tokenkey, user=user)
