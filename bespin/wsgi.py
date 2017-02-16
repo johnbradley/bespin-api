58321833079380a1318168b9192dd3e0640f0fb0
John Bradley <johnbradley2008@gmail.com>
2017-02-16 11:02:08 -0500

    separates out settings_base and adds settings_prod
    Production settings that specify postgres database and environment variables

--- bespin/wsgi.py
+++ bespin/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bespin.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bespin.settings_prod")
 
 application = get_wsgi_application()
