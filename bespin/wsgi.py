fa207eb69feb7fdee08b6c50060de67db9855316
John Bradley <johnbradley2008@gmail.com>
2017-02-16 11:41:10 -0500

    wsgi passes known env variables

--- bespin/wsgi.py
+++ bespin/wsgi.py
@@ -13,4 +13,21 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bespin.settings_prod")
 
-application = get_wsgi_application()
+_application = get_wsgi_application()
+
+env_variables_to_pass = [
+  'BESPIN_SECRET_KEY',
+  'BESPIN_ALLOWED_HOST',
+  'BESPIN_DB_NAME',
+  'BESPIN_DB_USER',
+  'BESPIN_DB_PASSWORD',
+  'BESPIN_DB_HOST',
+  'BESPIN_CORS_HOST',
+  'BESPIN_STATIC_ROOT,
+]
+
+def application(environ, start_response):
+    # pass the WSGI environment variables on through to os.environ
+    for var in env_variables_to_pass:
+        os.environ[var] = environ.get(var, '')
+    return _application(environ, start_response)
