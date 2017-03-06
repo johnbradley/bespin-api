c2867c3f49effcda4d4e4d380522aba05566b79f
John Bradley <johnbradley2008@gmail.com>
2017-03-06 12:49:35 -0500

    makeusertoken: delete old user token if necessary

--- data/management/commands/makeusertoken.py
+++ data/management/commands/makeusertoken.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
+from django.core.exceptions import ObjectDoesNotExist
 from rest_framework.authtoken.models import Token
 
 
@@ -21,16 +22,33 @@ class Command(BaseCommand):
         user.save()
         return user
 
+    @staticmethod
+    def _get_old_token(user):
+        try:
+            return Token.objects.get(user=user)
+        except ObjectDoesNotExist:
+            return None
+
+    def _delete_old_token(self, token):
+        tokenkey = token.key
+        token.delete()
+        self.stderr.write("Previous token '{}' deleted".format(tokenkey))
+
     def _create_token(self, user, tokenkey):
-        token, created = Token.objects.get_or_create(key=tokenkey, user=user)
-        if created:
-            self.stdout.write("Token '{}' created.".format(tokenkey))
-        else:
-            self.stderr.write("Token '{}' already exists".format(tokenkey))
+        Token.objects.create(key=tokenkey, user=user)
+        self.stdout.write("Token '{}' created.".format(tokenkey))
 
     def handle(self, **options):
         username = options['username']
         password = options['password']
         tokenkey = options['tokenkey']
         user = self._create_user(username, password)
-        self._create_token(user, tokenkey)
+        old_token = self._get_old_token(user)
+        if old_token:
+            if old_token.key == tokenkey:
+                self.stderr.write("Token '{}' already exists".format(tokenkey))
+            else:
+                self._delete_old_token(old_token)
+                self._create_token(user, tokenkey)
+        else:
+            self._create_token(user, tokenkey)
