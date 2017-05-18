39c40c5ad055ae02870a2028098bece598c151b0
John Bradley <johnbradley2008@gmail.com>
2017-05-18 11:44:50 -0400

    oauth endpoints and create cookie for bespin-ui
    adds authorize/code_callback and unconfigured endpoints under /auth/

--- bespin/urls.py
+++ bespin/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 from rest_framework.authtoken.views import obtain_auth_token
-from django.contrib.auth import views as auth_views
-from django.views.generic.base import RedirectView
+from django.views.generic.base import TemplateView
+from data import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -10,11 +10,7 @@ urlpatterns = [
     url(r'^api-auth/', include('rest_framework.urls',
                                namespace='rest_framework')),
     url(r'^api-auth-token/', obtain_auth_token),
-    url(r'^auth/', include('gcb_web_auth.urls')),
-    url(r'^accounts/login/$', auth_views.login, {'template_name': 'gcb_web_auth/login.html'}, name='login'),
-    url(r'^accounts/logout/$', auth_views.logout, {'template_name': 'gcb_web_auth/logged_out.html'}, name='logout'),
-    url(r'^accounts/login-local/$', auth_views.login, {'template_name': 'gcb_web_auth/login-local.html'},
-        name='login-local'),
-    # Redirect / to /accounts/login
-    url(r'^$', RedirectView.as_view(pattern_name='auth-home', permanent=False)),
+    url(r'^auth/authorize/$', views.authorize, name='auth-authorize'),
+    url(r'^auth/code_callback$', views.authorize_callback, name='auth-callback'),
+    url(r'^auth/unconfigured/$', TemplateView.as_view(template_name='gcb_web_auth/unconfigured.html'), name='auth-unconfigured'),
 ]
