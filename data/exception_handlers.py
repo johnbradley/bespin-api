cacea9f51a4c8e4868161d5f13b5043a7cf6d8e6
Dan Leehr <dan.leehr@duke.edu>
2017-07-11 14:11:42 -0400

    Prevent exception_handler from raising more exceptions when base handler returns None

--- data/exception_handlers.py
+++ data/exception_handlers.py
@@ -26,8 +26,13 @@ def json_root_object_exception_handler(exc, context):
     # Call REST framework's default exception handler first,
     # to get the standard error response.
     response = exception_handler(exc, context)
-    # response.data may be a list or a single object
+    # If exception_handler returns None, the exception has not been handled by rest_framework
+    # If we return None, Django responds with a 500 (and html in debug mode). So we should handle them and
+    # return appropriate responses. For now, we'll just return None from the handler to aid ongoing development
+    if response is None:
+        return None
 
+    # response.data may be list or a single object
     if isinstance(response.data, list):
         errors = [make_json_root_error(response.status_code, data, exc) for data in response.data]
     else:
