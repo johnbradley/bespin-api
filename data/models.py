8e664ec093109516897531aa70e97df9c08c7b79
John Bradley <johnbradley2008@gmail.com>
2017-09-20 16:04:42 -0400

    Adds WorkflowMethodsDocument
    One to one relationship to WorkflowVersion
    Filled in by the loadworkflow command
    two new arguments for load workflow
    1) url for jinja2 template
    2) filename to be used for the MethodsDocument (so we could switch formats html, txt, md)
       not sure filename is necessary if we can pick one format and stick with it
    Read only access via api

--- data/models.py
+++ data/models.py
@@ -78,6 +78,17 @@ class WorkflowVersion(models.Model):
         return '{} version: {} created: {}'.format(self.workflow.name, self.version, self.created)
 
 
+class WorkflowMethodsDocument(models.Model):
+    """
+    Methods document for a particular workflow version.
+    """
+    workflow_version = models.OneToOneField(WorkflowVersion, on_delete=models.CASCADE, null=False,
+                                            related_name='methods_document')
+    content = models.TextField(blank=False, null=False, help_text="Methods document contents.")
+    filename = models.CharField(max_length=255, blank=False, null=False,
+                                help_text="Filename to save the methods document into.")
+
+
 class JobFileStageGroup(models.Model):
     """
     Group of files to stage for a job
