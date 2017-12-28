3b8003625a4ccb1e30eaa3a4d048a582e78547ba
John Bradley <johnbradley2008@gmail.com>
2017-12-28 15:32:28 -0500

    Run unit tests under circleci
    Matching our current deployment versions:
    python 2.7
    postgres: 9.5

--- bespin/settings.py
+++ bespin/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 
 import os
 from bespin.settings_base import *
+import dj_database_url
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '+u!*o@^1!elg34r5u$z4xgq(_yq@fqw!d^4p6y*zh_o1buifsa'
@@ -32,6 +33,10 @@ DATABASES = {
         # 'ATOMIC_REQUESTS': True,
     }
 }
+# Update database configuration from $DATABASE_URL if set
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
+
 
 # So that pages served by ember development server are listed as OK to access this API
 CORS_ORIGIN_WHITELIST = (
