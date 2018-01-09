9e34668f3b6d60e0f1dd6dbd52cdcb4bc192b1d2
John Bradley <johnbradley2008@gmail.com>
2018-01-09 15:14:20 -0500

    fix package name for settings REST config

--- bespin/settings_base.py
+++ bespin/settings_base.py
@@ -120,11 +120,11 @@ REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer',
-        'drf_json_api.renderers.JSONRootObjectRenderer',
+        'drf_ember_backend.renderers.JSONRootObjectRenderer',
     ),
     'DEFAULT_PARSER_CLASSES': (
         'rest_framework.parsers.JSONParser',
-        'drf_json_api.parsers.JSONRootObjectParser',
+        'drf_ember_backend.parsers.JSONRootObjectParser',
     ),
-    'EXCEPTION_HANDLER': 'drf_json_api.exception_handlers.switching_exception_handler',
+    'EXCEPTION_HANDLER': 'drf_ember_backend.exception_handlers.switching_exception_handler',
 }
