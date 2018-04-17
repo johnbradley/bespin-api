f15b5bb65d32b722dc897dd27b79cbe8fdff32e0
Dan Leehr <dan.leehr@duke.edu>
2018-04-17 10:52:37 -0400

    Moves required group manager group out to settings/env
    - Group membership check is now only enforced if settings.REQUIRED_GROUP_MANAGER_GROUP is set

--- bespin/backend.py
+++ bespin/backend.py
@@ -1,13 +1,12 @@
 from gcb_web_auth.backends.oauth import OAuth2Backend
-
-BESPIN_USER_GROUP = 'duke:group-manager:roles:bespin-users'
+from django.conf import settings
 
 
 class BespinOAuth2Backend(OAuth2Backend):
     """
-    Adds check to make sure users belong to the bespin-user group manager group.
-    This group is managed via https://groups.oit.duke.edu/groupmanager/.
+    If required by settings, checks that user belongs to group-manager group
     """
     def check_user_details(self, details):
-        duke_unique_id = details['dukeUniqueID']
-        self.verify_user_belongs_to_group(duke_unique_id, BESPIN_USER_GROUP)
+        if settings.REQUIRED_GROUP_MANAGER_GROUP:
+            duke_unique_id = details['dukeUniqueID']
+            self.verify_user_belongs_to_group(duke_unique_id, settings.REQUIRED_GROUP_MANAGER_GROUP)
