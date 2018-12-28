9839d7aa00359a7e02e2f240f79396b0d8356b34
John Bradley <johnbradley2008@gmail.com>
2018-12-28 14:20:24 -0500

    simplify wsgi python

--- bespin/wsgi.py
+++ bespin/wsgi.py
@@ -11,22 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bespin.settings_prod")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bespin.settings")
 
-env_variables_to_pass = [
-  'BESPIN_SECRET_KEY',
-  'BESPIN_ALLOWED_HOST',
-  'BESPIN_DB_NAME',
-  'BESPIN_DB_USER',
-  'BESPIN_DB_PASSWORD',
-  'BESPIN_DB_HOST',
-  'BESPIN_CORS_HOST',
-  'BESPIN_STATIC_ROOT',
-]
-
-def application(environ, start_response):
-     # pass the WSGI environment variables on through to os.environ
-     for var in env_variables_to_pass:
-         os.environ[var] = environ.get(var, '')
-     _application = get_wsgi_application()
-     return _application(environ, start_response)
+application = get_wsgi_application()
